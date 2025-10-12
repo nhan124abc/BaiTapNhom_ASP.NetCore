3f28ac5cec516e499f3148b7e69236f788ede4b8
nhan124abc <caonhan15a@gmail.com>
2025-10-12 22:27:04 +0700

    ok

--- BaiTapNhom_2/obj/Debug/net8.0/BaiTapNhom_2.AssemblyInfo.cs
+++ BaiTapNhom_2/obj/Debug/net8.0/BaiTapNhom_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapNhom_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfdedb97ce2063d68032b42aba6f525d3503e225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f31ac4ad637bc70201f8f176d7825a9cc9f2447")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapNhom_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapNhom_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
